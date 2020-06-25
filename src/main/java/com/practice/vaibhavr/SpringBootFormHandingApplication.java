a332b957d4f795ac1e55f69175733502eb104ac9
Vaibhav Ranglani <ranglani.vaibhav@gmail.com>
2020-06-25 21:09:55 +0530

    Add working code to client app

---
+++ src/main/java/com/practice/vaibhavr/SpringBootFormHandingApplication.java
@@ -0,0 +1,15 @@
+package com.practice.vaibhavr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*Reference Example
+https://www.javainuse.com/spring/spring-boot-oauth-authorization-code*/
+@SpringBootApplication
+public class SpringBootFormHandingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFormHandingApplication.class, args);
+    }
+
+}
